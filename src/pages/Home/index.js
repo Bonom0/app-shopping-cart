59400bbe7397ae72b127bb36046899d4e3cbb8e8
Bonom0 <caioazbonomo@gmail.com>
2025-03-21 16:44:32 -0300

    Personalizado o header da página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,86 @@
-import { View, Text, StyleSheet } from "react-native"
+import { useState } from "react"
+import { View, Text, StyleSheet, SafeAreaView, FlatList, TouchableOpacity } from "react-native"
 
+import { Feather } from '@expo/vector-icons'
 
 export default function Home(){
+  const [products, setProducts] = useState([
+    {
+      id: '1',
+      name: "Coca cola",
+      price: 19.90
+    },
+    {
+      id: '2',
+      name: "Chocolate",
+      price: 6.50
+    },
+    {
+      id: '4',
+      name: "Queijo 500g",
+      price: 15
+    },
+    {
+      id: '5',
+      name: "Batata frita",
+      price: 23.90
+    },
+    {
+      id: '6',
+      name: "Guarana lata",
+      price: 6.00
+    },
+  ])
+  
   return(
-    <View style={styles.Container}>
-      <Text>Página Home</Text>
-    </View>
+    <SafeAreaView  style={styles.Container}>
+      <View style={styles.CartContent}>
+        <Text style={styles.Title}>Lista de produtos</Text>
+        
+        <TouchableOpacity style={styles.CartButton}>
+          <View style={styles.Dot}>
+            <Text style={styles.DotText}>3</Text>
+          </View>
+          <Feather name="shopping-cart" size={30} color="#000"/>
+        </TouchableOpacity>
+      </View>
+
+      
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   Container:{
-    flex: 1
+    flex: 1,
+    backgroundColor: '#FAFAFA',
+    paddingEnd: 14,
+    paddingStart: 14
+  },
+  CartContent:{
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 24,
+    marginBottom: 24
+  },
+  Title:{
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  Dot:{
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "red",
+    width: 20,
+    height: 20,
+    borderRadius: 12,
+    position: "absolute", //fica sobre o ícone do cart
+    zIndex: 99, //faz com que a dot fique em cima do carrinha e não atrás
+    bottom: -2,
+    left: -4
+  },
+  DotText:{
+    fontSize: 12
   }
 })
