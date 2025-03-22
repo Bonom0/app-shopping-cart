a4cda952d638b5794da1fd8af7b3a5c5f8875d85
Bonom0 <caioazbonomo@gmail.com>
2025-03-22 10:16:06 -0300

    Corrigido a importação da pasta contexts

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes';
-import CartProvider from './src/context/CartContext';
+import CartProvider from './src/contexts/CartContext';
 
 export default function App() {
   return (
